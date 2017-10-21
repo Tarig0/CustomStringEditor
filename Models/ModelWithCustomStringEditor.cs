1fe0cc0dd25ca860b3a7780939060f458050c8c8
Allen Cook <alcook16@gmail.com>
2017-10-20 20:15:45 -0400

    initial commit

---
+++ Models/ModelWithCustomStringEditor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomStringEditorInNullModel.Models
+{
+    public class ModelWithCustomStringEditor
+    {
+        [UIHint("Custom")]
+        public string SectionedKey { get; set; }
+        public string hello { get; set; } = "I am in editor";
+    }
+}
---
+++ Models/ParentModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomStringEditorInNullModel.Models
+{
+    public class ParentModel
+    {
+        public IEnumerable<ModelWithCustomStringEditor> ModelWithCustomStringEditors { get; set; }
+        public string Name { get; set; }
+    }
+}
